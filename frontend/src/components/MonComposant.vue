ffee7ac44132e373076b7fa6c121624d57735777
Quentin Goulvestre <goulvestrequentin@gmail.com>
2020-02-27 18:59:10 +0100

    add MonComposant plus add/mon_composant route plus add link to app

---
+++ frontend/src/components/MonComposant.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="mon-composant">
+  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MonComposant',
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.mon-composant{
+  background: red;
+  width: 100%;
+  height: 100px;
+}
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -18,6 +18,11 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
+  {
+    path: '/mon_composant',
+    name: 'MonComposant',
+    component: () => import('../components/MonComposant.vue'),
+  },
 ];
 
 const router = new VueRouter({
